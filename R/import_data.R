710e2bd418b80583f23f290a0a9b61f7bf57e029
Ratnaweera Nils (rata) <rata@zhaw.ch>
2018-11-14 10:37:57 +0100

    third

--- R/import_data.R
+++ R/import_data.R
@@ -1,28 +1,28 @@
 
 
-library(sf)
-library(dplyr)
+require(sf)
+require(dplyr)
 
 
-read_sf("inst/extdata/swissBOUNDARIES3D_1_3_TLM_BEZIRKSGEBIET.shp") %>%
+sf::read_sf("inst/extdata/swissBOUNDARIES3D_1_3_TLM_BEZIRKSGEBIET.shp") %>%
   st_set_crs(2056) %>%
   save(file = "data/berzirksgebiet.RData")
 
 
-read_sf("inst/extdata/swissBOUNDARIES3D_1_3_TLM_HOHEITSGEBIET.shp") %>%
+sf::read_sf("inst/extdata/swissBOUNDARIES3D_1_3_TLM_HOHEITSGEBIET.shp") %>%
   st_set_crs(2056) %>%
   save(file = "data/hoheitsgebiet.RData")
 
 
-read_sf("inst/extdata/swissBOUNDARIES3D_1_3_TLM_HOHEITSGRENZE.shp") %>%
+sf::read_sf("inst/extdata/swissBOUNDARIES3D_1_3_TLM_HOHEITSGRENZE.shp") %>%
   st_set_crs(2056) %>%
   save(file = "data/hoheitsgrenze.RData")
 
-read_sf("inst/extdata/swissBOUNDARIES3D_1_3_TLM_KANTONSGEBIET.shp") %>%
+sf::read_sf("inst/extdata/swissBOUNDARIES3D_1_3_TLM_KANTONSGEBIET.shp") %>%
   st_set_crs(2056) %>%
   save(file = "data/kantonsgebiet.RData")
 
 
-read_sf("inst/extdata/swissBOUNDARIES3D_1_3_TLM_LANDESGEBIET.shp") %>%
+sf::read_sf("inst/extdata/swissBOUNDARIES3D_1_3_TLM_LANDESGEBIET.shp") %>%
   st_set_crs(2056) %>%
   save(file = "data/landesgebiet.RData")
