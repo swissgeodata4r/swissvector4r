dc2648ba0e74749df55a3e7a531ee04f5efde76a
Ratnaweera Nils (rata) <rata@zhaw.ch>
2018-11-14 13:18:52 +0100

    changed package name, version and description

--- data-raw/import_data.R
+++ data-raw/import_data.R
@@ -1,39 +1,32 @@
 
 
 require(sf)
-require(dplyr)
 
-
-berzirksgebiet <- sf::read_sf("data-raw/swissBOUNDARIES3D_1_3_TLM_BEZIRKSGEBIET.shp") %>%
+bezirksgebiet <- sf::read_sf("data-raw/swissBOUNDARIES3D_1_3_TLM_BEZIRKSGEBIET.shp") %>%
   st_set_crs(2056)
 
 
-# save(berzirksgebiet, file = "data/berzirksgebiet.RData")
-usethis::use_data(berzirksgebiet, overwrite = TRUE,compress = "xz")
+usethis::use_data(bezirksgebiet, overwrite = TRUE,compress = "xz")
 
 
 hoheitsgebiet <- sf::read_sf("data-raw/swissBOUNDARIES3D_1_3_TLM_HOHEITSGEBIET.shp") %>%
   st_set_crs(2056)
 
-# save(hoheitsgebiet, file = "data/hoheitsgebiet.RData")
 usethis::use_data(hoheitsgebiet, overwrite = TRUE,compress = "xz")
 
 
 hoheitsgrenze <- sf::read_sf("data-raw/swissBOUNDARIES3D_1_3_TLM_HOHEITSGRENZE.shp") %>%
   st_set_crs(2056)
 
-# save(hoheitsgrenze, file = "data/hoheitsgrenze.RData")
 usethis::use_data(hoheitsgrenze, overwrite = TRUE,compress = "xz")
 
 kantonsgebiet <- sf::read_sf("data-raw/swissBOUNDARIES3D_1_3_TLM_KANTONSGEBIET.shp") %>%
   st_set_crs(2056)
 
-# save(kantonsgebiet, file = "data/kantonsgebiet.RData")
 usethis::use_data(kantonsgebiet, overwrite = TRUE,compress = "xz")
 
 
 landesgebiet <- sf::read_sf("data-raw/swissBOUNDARIES3D_1_3_TLM_LANDESGEBIET.shp") %>%
   st_set_crs(2056)
 
-# save(landesgebiet, file = "data/landesgebiet.RData")
 usethis::use_data(landesgebiet, overwrite = TRUE,compress = "xz")
